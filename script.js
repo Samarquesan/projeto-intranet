efaecfd51df0ba42d44e12fe494eea156cea555a
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-11-01 20:29:58 -0300

    Atualização da parte logica e inclusão de midia query

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
+const usuario = document.getElementById('IdUsuario').value;
+const email = document.getElementById('IdEmail').value;
+const senha = document.getElementById('IdSenha').value;
+
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
@@ -19,3 +23,66 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+
+function logar(){
+    var login = document.getElementById('IdEmail').value;
+    var senha = document.getElementById('IdSenha').value;
+
+    if(login == "0110adm" && senha == "adm1001"){
+        alert('Sucesso!');
+        location.href = "home.html";
+    }else{
+        alert('Usuário ou senha incorretos!');
+    }
+}
+
+
+
+const registros = [
+    {
+        nome: "Samuel Marques dos Santos",
+        email: "samuel_simic@hotmail.com",
+        senha: "sam123"
+    }
+];
+
+//Crirar Registro (CREATE)
+function cadastrar(usuario, email, senha){
+   registros = {nome: nome, email: email, senha: senha};
+
+    
+}
+
+
+//Ver registros (READ)
+function verRegistros() {
+    console.log(registros);
+    return registros;
+}
+
+
+
+
+//Atualizar cadastros (UPDATE)
+function atualizaCadastro(nome, novoEmail, novaSenha) {
+    const user = registros.findIndex(user => user.nome == nome);
+
+    if(novoValor){
+        produtos[user].email = novoEmail;
+    }
+    if(novoEstoque){
+        produtos[user].senha = novaSenha;
+    }
+};
+
+//Excluir registros(DELETE)
+function excluirCadastro(nome){
+    const user = produtos.findIndex(user => user.nome == nome);
+    registros.splice(user, 1);
+}
+
+cadastrar("Samuel Braga da Silva", "sam.sambragasil@gmail", "bragasil1212");
+//atualizaCadastro("Samuel Braga da Silva", "sam.sambragasil@gmail", "braga111222")
+//excluirCadastro("Samuel Braga da Silva");
+verRegistros();
